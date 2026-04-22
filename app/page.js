a8fc4e927b267183c1f93e82587143271dd369af
stacknerdjoe <joseph.onyenemerem@yahoo.com>
2026-04-22 11:54:44 +0200

    fixed base URl issue

--- app/page.js
+++ app/page.js
@@ -1,37 +1,46 @@
 import ImageBanner from "@/components/ImageBanner";
 import Products from "@/components/Products";
+import Stripe from "stripe";
 
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY)
 
-export async function getProducts() {
-    const baseURL = process.env.NEXT_PUBLIC_BASE_URL
-    const response = await fetch(baseURL + '/api/products')
-    const products = await response.json()
-    return products
+async function getProducts() {
+    const products = await stripe.products.list({ active: true })
+    const prices = await stripe.prices.list({ active: true })
+
+    return products.data.map((product) => ({
+        ...product,
+        prices: prices.data
+            .filter((price) => price.product === product.id)
+            .map((price) => ({
+                id: price.id,
+                unit_amount: price.unit_amount,
+                currency: price.currency,
+                recurring: price.recurring
+            }))
+    }))
 }
 
-export default async function Home(props) {
-
+export default async function Home() {
     const products = await getProducts()
-  
 
     let planner = null
     let stickers = []
-    
+
     for (let product of products) {
-      if (product.name === 'Quest & Campaigns Planner') {
-        planner = product
-        continue
-      }
-      stickers.push(product)
+        if (product.name === 'Quest & Campaigns Planner') {
+            planner = product
+            continue
+        }
+        stickers.push(product)
     }
 
-
     return (
-      < >
-        <ImageBanner/>
-        <section>
-          <Products planner={planner} stickers={stickers} />
-      </section>
-    </>
-  );
+        <>
+            <ImageBanner/>
+            <section>
+                <Products planner={planner} stickers={stickers} />
+            </section>
+        </>
+    );
 }
